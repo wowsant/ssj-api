c3e5bfc9cb4768533b318a30d1b806e81da58f91
sant.silva <sant.silva@localiza.com>
2021-12-09 09:12:13 -0300

    Teste web heroku

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('/documentos-web', DocumentoController::class);
